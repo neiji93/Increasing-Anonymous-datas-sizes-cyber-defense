79d09b828fd58377a80008afe95fd94269e6e01f
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-03 19:12:44 +0200

    Update ChemicalSognalHash.h

--- ChemicalSognalHash.h
+++ ChemicalSognalHash.h
@@ -1,1 +1,4 @@
 //this chemiczl signal may repear a little bit rapidely.
+//there are also at least 5 different types of signals.
+//but to manage the hd, we need to transform commands as 11 digits (personal table).
+//so we need a model.
