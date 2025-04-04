1076054208c3c27e3e9b6fb71abf1a60c94e8408
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-04-04 07:11:54 +0200

    Create DefragSchedullerKernell.h

---
+++ DefragSchedullerKernell.h
@@ -0,0 +1,11 @@
+//as we deal with 121ko blank files, it is possible 
+//to use the kernel methods. and as we have async and coroutine capabilities for
+//kernel mode (my other projects), we can use a defragmenter.
+
+class DummyOne{
+public methodo'e(void* parametrrs); //old stuffs must be good here because we do't have to change
+                                    //the nasic behaviour of a real defrag.
+}
+
+void* methodonePreventPatch(void* parameters); //cannot be used with signals.
+void
